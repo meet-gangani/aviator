749d6bea7e471331d297d054f01733f526d1d321
Meet Gangani <meetgangani71@gmail.com>
2023-03-07 23:29:55 +0530

    - Added plan img

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './App.css'
 import { Area, AreaChart, CartesianGrid, Customized, ResponsiveContainer } from 'recharts'
 import Plan1 from './assets/images/plane-1.svg'
 
@@ -6,32 +7,11 @@ function App() {
   const [ chartData, setChartData ] = useState([])
 
   // const updateChartData = () => {
-    // newChartData.push(Math.random());
-    // setChartData((prevValue) => [ ...prevValue, Math.random() ])
+  // newChartData.push(Math.random());
+  // setChartData((prevValue) => [ ...prevValue, Math.random() ])
   // const newChartData = [...chartData];
   // setChartData((prevValue) => [ ...prevValue, Math.random() ])
   // }
-
-  const MovingObject = ({ x, y, value }) => (
-      <text x={x} y={y} textAnchor="middle">
-        {value}
-      </text>
-  )
-
-  const [ movingObject, setMovingObject ] = useState({
-    name: 'Jun',
-    value: 0
-  })
-
-  const handleMouseMove = (event) => {
-    const mouseX = event.chartX
-    const xScale = event.activeLabelProps.scale.x
-    const xValue = xScale.invert(mouseX)
-    setMovingObject({
-      name: xValue,
-      value: Math.floor(Math.random() * 100)
-    })
-  }
 
   useEffect(() => {
     const step = 0.1
@@ -50,60 +30,90 @@ function App() {
     // return () => clearInterval(intervalId);
   }, [])
 
+  const MovingObject = ({ x, y, value }) => (
+      <text x={x} y={y} textAnchor="middle">
+        {value}
+      </text>
+  )
+
+  const [ movingObject, setMovingObject ] = useState({
+    name: 'Jun',
+    value: 0
+  })
+
+  const handleMouseMove = (event) => {
+    const mouseX = event.chartX
+    const xScale = event.activeLabelProps.scale.x
+    const xValue = xScale.invert(mouseX)
+
+    setMovingObject({
+      name: xValue,
+      value: Math.floor(Math.random() * 100)
+    })
+  }
+
   const renderImage = (props) => {
     return (
         <image
-            x={20}
-            y={20}
-            width={40}
-            height={40}
-              src={Plan1}
+            className="plan"
+            x={30}
+            y={580}
+            width={150}
+            height={74}
+            href={Plan1}
         />
     )
   }
 
   return (
-      // <img src="./assets/images/plane-1.svg" alt=""/>
-      <ResponsiveContainer width={800} height={800}>
-        <AreaChart data={chartData}>
-          <CartesianGrid strokeDasharray="3 3"/>
-          {/*<Animation duration={1000}/>*/}
-          <Area type="monotone" dataKey="value" animationDuration={15000} stroke="#8884d8" fill="#8884d8"/>
-          {/*<ForeignObject x="50%" y="50%" width="200" height="200">*/}
-          {/*  <div xmlns="http://www.w3.org/1999/xhtml">*/}
-          {/*    <h1>Hello world</h1>*/}
-          {/*  </div>*/}
-          {/*</ForeignObject>*/}
-          <Customized component={renderImage}/>
-        </AreaChart>
-      </ResponsiveContainer>
+      <>
+        {/*<img src="./assets/images/plane-1.svg" alt=""/>*/}
+        <ResponsiveContainer width={800} height={800}>
+          <AreaChart data={chartData}>
+            <CartesianGrid strokeDasharray="3 3"/>
+            {/*<Animation duration={1000}/>*/}
+            <Area type="monotone" dataKey="value" animationDuration={15000} stroke="#e50539" fill="#e50539"/>
+            {/*<ForeignObject x="50%" y="50%" width="200" height="200">*/}
+            {/*  <div xmlns="http://www.w3.org/1999/xhtml">*/}
+            {/*    <h1>Hello world</h1>*/}
+            {/*  </div>*/}
+            {/*</ForeignObject>*/}
+            <Customized component={renderImage}/>
+            {/*<Customized component={<h1 className="bg_glow">hello</h1>}/>*/}
+          </AreaChart>
+        </ResponsiveContainer>
 
-      // <LineChart width={1800} height={800} data={chartData} onMouseMove={handleMouseMove}>
-      //   <CartesianGrid strokeDasharray="3 3" />
-      //   <XAxis dataKey="name" />
-      //   <YAxis />
-      //   <Line animationDuration={5000} type="monotone" dataKey="value" stroke="#8884d8" />
-      //   <Label
-      //       content={<MovingObject value={movingObject.value} />}
-      //       position="top"
-      //       offset={10}
-      //       render={({ viewBox }) => (
-      //           <MovingObject
-      //               x={viewBox.x + (viewBox.width / (chartData.length - 1)) * chartData.findIndex((d) => d.name === movingObject.name)}
-      //               y={viewBox.y}
-      //               value={movingObject.value}
-      //           />
-      //       )}
-      //   />
-      // </LineChart>
+        <div className="glow">
+          <span className="bg_glow">123</span>
+        </div>
 
-      // <LineChart width={500} height={300} data={chartData}>
-      //   <Line type="monotone" dataKey="value" stroke="#8884d8" />
-      //   <CartesianGrid stroke="#ccc" />
-      //   <XAxis dataKey="name" />
-      //   <YAxis />
-      //   <Customized component={renderImage} />
-      // </LineChart>
+        {/* <LineChart width={1800} height={800} data={chartData} onMouseMove={handleMouseMove}>*/}
+        {/*   <CartesianGrid strokeDasharray="3 3" />*/}
+        {/*   <XAxis dataKey="name" />*/}
+        {/*   <YAxis />*/}
+        {/*   <Line animationDuration={5000} type="monotone" dataKey="value" stroke="#8884d8" />*/}
+        {/*   <Label*/}
+        {/*       content={<MovingObject value={movingObject.value} />}*/}
+        {/*       position="top"*/}
+        {/*       offset={10}*/}
+        {/*       render={({ viewBox }) => (*/}
+        {/*           <MovingObject*/}
+        {/*               x={viewBox.x + (viewBox.width / (chartData.length - 1)) * chartData.findIndex((d) => d.name === movingObject.name)}*/}
+        {/*               y={viewBox.y}*/}
+        {/*               value={movingObject.value}*/}
+        {/*           />*/}
+        {/*       )}*/}
+        {/*   />*/}
+        {/* </LineChart>*/}
+
+        {/* <LineChart width={500} height={300} data={chartData}>*/}
+        {/*   <Line type="monotone" dataKey="value" stroke="#8884d8" />*/}
+        {/*   <CartesianGrid stroke="#ccc" />*/}
+        {/*   <XAxis dataKey="name" />*/}
+        {/*   <YAxis />*/}
+        {/*   <Customized component={renderImage} />*/}
+        {/* </LineChart>*/}
+      </>
   )
 }
 
